24b67432125f56ce024dedba2845d2bfe47f7f79
Sadiul Hakim <sadiulhakim@gmail.com>
2025-12-29 17:07:08 +0600

    URL

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,6 @@
 <h1>About</h1>
 <p>{{ $text }}</p>
+
+<p>
+    {{ URL::previous() }}
+</p>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 <h1>Hi I am learning PHP.</h1>
 <a href="/about/user">About Page</a>
+<a href="{{ URL::to('about-me', ['HK']) }}">Another About Page</a>
 
 {{ random_int(1, 100) }}
 
@@ -13,3 +14,8 @@
 {{-- @includeIf('common.header', ['owner' => 'HakimSoftware']) --}}
 
 <x-alert msg="User Login successful" /> {{-- component, we can pass `class` as well --}}
+
+<p>{{ URL::current() }}</p>
+<p>{{ URL::full() }}</p>
+<p>{{ url()->current() }}</p>
+<p>{{ url()->full() }}</p>
